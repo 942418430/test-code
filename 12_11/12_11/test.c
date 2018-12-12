9cf852e79c770fd34859ffe942af8740d3defd21
942418430 <942418430@qq.com>
2018-12-12 22:05:25 +0800

    提交代码

---
+++ 12_11/12_11/test.c
@@ -0,0 +1,104 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("a=%d,b=%d,c=%d\n", a, b, c);//-1,-1,255                                                                                                                                                                                                                                                                                                                                                                   
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);//4294967168
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	char a = 128;
+//	printf("%u\n", a);//4294967168
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int i = -20;
+//	unsigned int j = 10;
+//	printf("%d\n", i + j);//-10
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);//À¿—≠ª∑
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 100; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d\n", strlen(a));//1011
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	short num = 32767;
+//	short int a = num + 1;
+//	printf("%d\n", a);//-32768
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//unsigned char i = 0;
+//
+//int main()
+//{
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello wolrd\n");//À¿—≠ª∑
+//	}
+//	system("pause");
+//	return 0;
+//}
+
